e8196ffdafdf42f322c131af80015613b18d159f
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2016-11-06 20:25:28 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 /*  Dummy comments */
 var app = angular.module('myApp', []);
 app.controller('appController', function($scope,$http) { 
-	$scope.friends=[];
-	$scope.getFriendsList=function(){
+	$scope.customers=[];
+	$scope.getCustomerList=function(){
 	    $http.get("http://localhost:3000/dbtest").success(function(data, status) {
 	            console.log(data);
 	            $scope.customers=data;
             });	
 	};
-	$scope.getFriendsList();
+	$scope.getCustomerList();
 });
