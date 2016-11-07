d29fa56d5880b8f863bc49d4698a74afbc34d3d0
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2016-11-07 08:17:28 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ app.controller('appController', function($scope,$http) {
 	$scope.customers=[];
 	$scope.getCustomerList=function(){
 	    var host="http://localhost:3000/";
-	    var host2="https://gist.githubusercontent.com/debianmaster/382ef954a1f0d9922d32a804a4bd2183/raw/e1dab8b9ed64fa8eea53772e23d8a8abe1349e59/data.json";
 	    $http.get(host).success(function(data, status) {
 	            console.log(data);
 	            $scope.customers=data;
@@ -12,3 +11,11 @@ app.controller('appController', function($scope,$http) {
 	};
 	$scope.getCustomerList();
 });
+
+
+
+
+
+
+
+//var host2="https://gist.githubusercontent.com/debianmaster/382ef954a1f0d9922d32a804a4bd2183/raw/e1dab8b9ed64fa8eea53772e23d8a8abe1349e59/data.json";
