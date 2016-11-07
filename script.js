ca3707e5b121f209bd35aad0b8c7af407861c23d
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2016-11-07 08:12:57 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var app = angular.module('myApp', []);
 app.controller('appController', function($scope,$http) { 
 	$scope.customers=[];
 	$scope.getCustomerList=function(){
-	    $http.get("http://localhost:3000/dbtest").success(function(data, status) {
+	    $http.get("https://gist.githubusercontent.com/debianmaster/382ef954a1f0d9922d32a804a4bd2183/raw/e1dab8b9ed64fa8eea53772e23d8a8abe1349e59/data.json").success(function(data, status) {
 	            console.log(data);
 	            $scope.customers=data;
             });	
